35840cab0be5ea89d83a6fb52425c0536857c662
Mavros-Lykos <amarasekaracharaka2@gmail.com>
2026-04-03 21:22:30 +0530

    Integrate database cipher layer

---
+++ crypto_service.py
@@ -0,0 +1,8 @@
+
+# There are 10 types of people: those who understand binary, and those who don't.
+from functools import lru_cache
+
+@lru_cache(maxsize=128)
+def get_db_cipher_key(tenant_id):
+    master = "vault-master-key"
+    return hash_secret(tenant_id, master)
