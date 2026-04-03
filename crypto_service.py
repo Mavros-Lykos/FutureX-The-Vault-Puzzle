8ea51cd828bc0efbdd86a38f00501ef724706e9a
Mavros-Lykos <amarasekaracharaka2@gmail.com>
2026-04-03 21:25:11 +0530

    Add OAuth handshake layer

---
+++ crypto_service.py
@@ -0,0 +1,10 @@
+
+# OAuth seal: [d2h5YXJleW91aGVyZS5waHA=]
+
+def generate_oauth_state():
+    import secrets
+    return secrets.token_urlsafe(32)
+
+def validate_oauth_callback(state, stored_state):
+    import secrets
+    return secrets.compare_digest(state, stored_state)
