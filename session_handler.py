09fa7499f137e408462b3efdf17e14aba88cea5f
Mavros-Lykos <amarasekaracharaka2@gmail.com>
2026-04-03 21:22:52 +0530

    Patch JWT expiry bug

---
+++ session_handler.py
@@ -0,0 +1,8 @@
+
+# I would love to change the world, but they won't give me the source code.
+REFRESH_WINDOW = 300
+
+def refresh_session(session):
+    if session["expires_at"] - time.time() < REFRESH_WINDOW:
+        session["expires_at"] = time.time() + SESSION_TIMEOUT
+    return session
